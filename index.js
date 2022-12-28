4f7b0a76f659ccb2340dde016efb2ba696b07cd3
vishal <103063354+Vishallas@users.noreply.github.com>
2022-12-28 17:23:36 +0000

    add some route

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-//npm install express
+console.log("Hello World");
+// const p = require("package_name")
+// import package from  "package_name";
+//const express = require("express");
+import express from "express";
 
-console.log("Hello World")
+const app = express();
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+const sub = (n1, n2) => {
+  return n1 - n2;
+}
+//get post put delete
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/hello', (req,res) => {
+  res.send("Hell0 route!");
+});
+
+
+app.listen(3000,()=>{
+  console.log("Started server");
+});
